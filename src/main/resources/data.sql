edc38cb3e40207465da90f6b0683b5058f27720c
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-09-26 15:00:28 -0300

    Foto do Produto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Produto(id, nome, descricao, fotoUrl, dataCadastro, dataUltimaAtualizacao, valorUnitario)
-VALUES(1, 'Moto G5 Plus', 'Celular com rede 5G', '', '2021-09-22', CURRENT_TIMESTAMP(), 1799.00);
+VALUES(1, 'Moto G5 Plus', 'Celular com rede 5G', 'https://a-static.mlcdn.com.br/1500x1500/capa-anti-shock-pelicula-de-gel-para-motorola-moto-g-5g-plus-fse-acessorios/fseacessorios/649007779/34a012d10687d5687c256baf47255f76.jpg', '2021-09-22', CURRENT_TIMESTAMP(), 1799.00);
 
 INSERT INTO Produto(id, nome, descricao, fotoUrl, dataCadastro, dataUltimaAtualizacao, valorUnitario)
-VALUES(2, 'Samsung Galaxy S9', 'Nova versão da Samsung', '', '2021-09-21', CURRENT_TIMESTAMP(), 1899.00);
+VALUES(2, 'Samsung Galaxy S9', 'Nova versão da Samsung', 'https://www.maniavirtual.com.br/content/images/thumbs/0072663_600.gif', '2021-09-21', CURRENT_TIMESTAMP(), 1899.00);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Produto (
     id INT NOT NULL,
     nome VARCHAR(50) NOT NULL,
     descricao VARCHAR(200),
-    fotoUrl VARCHAR(400),
+    fotoUrl VARCHAR(600),
     dataCadastro DATE,
     dataUltimaAtualizacao TIMESTAMP,
     valorUnitario FLOAT(5.2)
